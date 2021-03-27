ac05dd26f386ab4970ab1010002664db776d43fd
Artsem <45534369+nicedevtryhard@users.noreply.github.com>
2021-03-27 12:30:03 +0300

    feat: add transform-array func

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("arr is not an Array");
+  }
+  const cases = {
+    "--double-next": 1,
+    "--double-prev": 2,
+    "--discard-next": 3,
+    "--discard-prev": 4,
+  };
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (cases[arr[i]]) {
+      switch (cases[arr[i]]) {
+        case 1:
+          if (i !== arr.length - 1) {
+            newArr.push(arr[i + 1]);
+          }
+          break;
+        case 2:
+          if (i > 0 && arr[i - 2] !== "--discard-next") {
+            newArr.push(arr[i - 1]);
+          }
+          break;
+        case 3:
+          if (i !== arr.length - 1) {
+            i++;
+          }
+          break;
+        case 4:
+          if (i > 0 && arr[i - 2] !== "--discard-next") {
+            newArr.pop();
+          }
+          break;
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
